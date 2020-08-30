a47ee04fb84ad24c2ed603cfe85e251ca9b6f431
JorginhoRavello <a2racer6@gmail.com>
2020-08-30 18:12:48 -0500

    Se creo los componentes header, sidebar y footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+ 
+</script>
+
+<div class="Footer">
+    <div class="Footer-container">
+        <div class="Footer-copy">
+            © 2020 PUGSTAGRAM FROM PLATZI
+        </div>
+    </div>
+</div>
+
+<style>
+    
+.Footer-copy {
+    display: inline-block;
+    text-align: center;
+    margin: 0px;
+}
+</style>
---
+++ src/components/Stories.svelte
@@ -0,0 +1,19 @@
+<div class="Stories">
+	<div class="Stories-cont">
+			<div class="Stories-head">
+					<h2>Historias</h2>
+					<span>Ver todas</span>
+			</div>
+			<div class="Stories-items">
+					<div class="Stories-item">
+							<div class="Stories-item-box">
+									<img src="https//arepa.s3.amazonaws.com/oscar.png" alt="" />
+							</div>
+							<h2>
+									JorxD
+									<pan>10 horas antes</pan>
+							</h2>
+					</div>
+			</div>
+	</div>
+</div>
