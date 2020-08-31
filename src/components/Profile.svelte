92837f96d7c22771c73289959d7beab3a05d8df7
JorginhoRavello <a2racer6@gmail.com>
2020-08-30 19:12:22 -0500

    Se creo componente Card comments y se ingreso codigo TimeLine

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,11 +1,11 @@
 <div class="Profile">
     <div class="Profile-content">
         <div class="Profile-avatar">
-            <img src="https//arepa.s3.amazonaws.com/oscar.png" alt="" />
+            <img src="https://i.pinimg.com/564x/0b/d1/a1/0bd1a1b9a87c8ebd76ddc936a743c2ad.jpg" alt="" />
         </div>
         <div class="Profile-info">
-            <h2>JorxD</h2>
-            <span>Jorginho ravello</span>
+            <h2>Kirito</h2>
+            <span>kirigaya kazuto</span>
         </div>
     </div>
 </div>
--- src/components/Stories.svelte
+++ src/components/Stories.svelte
@@ -7,7 +7,7 @@
 			<div class="Stories-items">
 					<div class="Stories-item">
 							<div class="Stories-item-box">
-									<img src="https//arepa.s3.amazonaws.com/oscar.png" alt="" />
+									<img src="https://i.pinimg.com/564x/0b/d1/a1/0bd1a1b9a87c8ebd76ddc936a743c2ad.jpg" alt="" />
 							</div>
 							<h2>
 									JorxD
